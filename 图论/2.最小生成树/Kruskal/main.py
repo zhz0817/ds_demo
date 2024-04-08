114c80bbaf82fdea2368c75c5f647947a128b194
zhz0817 <zhz15114585761@gmail.com>
2024-04-08 10:43:06 +0800

    最小生成树

--- 图论/2.最小生成树/Kruskal/main.py
+++ 图论/2.最小生成树/Kruskal/main.py
@@ -1,1 +1,42 @@
 # https://leetcode.cn/problems/min-cost-to-connect-all-points/solutions/565990/prim-and-kruskal-by-yexiso-c500/
+# https://leetcode.cn/problems/min-cost-to-connect-all-points/description/
+
+from typing import List
+
+
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        start = 0
+        n = len(points)
+        ans = 0
+        graph = []
+        for i in range(n):
+            temp = [0] * n
+            graph.append(temp)
+        for i in range(n):
+            for j in range(i + 1, n):
+                dist = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])
+                graph[i][j] = dist
+                graph[j][i] = dist
+        min_dist = [0x3f3f3f3f] * n
+        is_visited = [False] * n
+        is_visited[start] = True
+        for i in range(n):
+            if i == start:
+                continue
+            min_dist[i] = graph[i][start]
+        count = 1
+        while count < n:
+            count += 1
+            min_index = -1
+            min_val = 0x3f3f3f
+            for i in range(n):
+                if not is_visited[i] and min_dist[i] < min_val:
+                    min_index = i
+                    min_val = min_dist[i]
+            is_visited[min_index] = True
+            ans += min_val
+            for i in range(n):
+                if not is_visited[i] and graph[i][min_index] < min_dist[i]:
+                    min_dist[i] = graph[i][min_index]
+        return ans
--- 图论/2.最小生成树/Prim/main.py
+++ 图论/2.最小生成树/Prim/main.py
@@ -1,1 +1,57 @@
 # https://leetcode.cn/problems/min-cost-to-connect-all-points/solutions/565990/prim-and-kruskal-by-yexiso-c500/
+from typing import List
+class UnionFind:
+    def __init__(self, n: int):
+        self.n = n
+        self.parents = [0] * n
+        for i in range(n):
+            self.parents[i] = i
+
+    def find(self, x: int) -> int:
+        if self.parents[x] == x:
+            return x
+        return self.find(self.parents[x])
+
+    def union(self, x: int, y: int):
+        root_x = self.find(x)
+        root_y = self.find(y)
+        if root_x != root_y:
+            self.parents[root_x] = root_y
+
+    def is_union(self, x: int, y: int) -> bool:
+        return self.find(x) == self.find(y)
+
+
+class Edge:
+    def __init__(self, node1: int, node2: int, weight: int):
+        self.node1 = node1
+        self.node2 = node2
+        self.weight = weight
+
+
+def getDist(points: List[List[int]], x: int, y: int) -> int:
+    return abs(points[x][0] - points[y][0]) + abs(points[x][1] - points[y][1])
+
+
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        n = len(points)
+        uf = UnionFind(n)
+        list1 = []
+        for i in range(n):
+            for j in range(i + 1, n):
+                list1.append(Edge(i, j, getDist(points, i, j)))
+        ans = 0
+        count = 1
+        list1 = sorted(list1, key=lambda x: x.weight, reverse=False)
+        for info in list1:
+            x = info.node1
+            y = info.node2
+            weight = info.weight
+            if not uf.is_union(x, y):
+                ans += weight
+                count += 1
+                uf.union(x, y)
+                if count == n:
+                    break
+        return ans

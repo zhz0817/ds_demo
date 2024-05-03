bfae3b645e3d3dd2142e5cc8a3362e23f16393e1
zhz <zhz15114585761@gmail.com>
2024-05-03 09:02:10 +0800

    循环链表

--- 链表/循环链表/main.py
+++ 链表/循环链表/main.py
@@ -5,47 +5,48 @@ class ListNode:
 
 
 class LinkList:
-    def __init__(self):
-        self.head = None
+    def __init__(self):  # self.head.next = self.tail 是一定要的，否则插入那里要写if
+        self.head = ListNode(-1)
+        self.tail = self.head
+        self.head.next = self.tail
         self.length = 0
 
     def clear(self):
-        self.head = None
+        self.head = ListNode(-1)
+        self.tail = self.head
+        self.head.next = self.tail
         self.length = 0
-        # self.__init__() #因为构造函数会返回对象
 
     def is_empty(self):
         return self.length == 0
 
-    def append_first(self, data):
+    def append_last(self, data):
         node = ListNode(data=data)
-        if self.head is None:
-            self.head = node
-        else:
-            node.next = self.head
-            self.head = node
+        self.tail.next = node
+        self.tail = self.tail.next
+        self.tail.next = self.head
         self.length += 1
 
-    def get_length(self):
+    def get_length(self) -> int:
         return self.length
 
     def get_value_by_index(self, index) -> int:
-        temp = self.head
         if index < 0 or index >= self.length:
             raise IndexError("Index out of")
+        temp = self.head.next
         while index > 0 and temp is not None:  # temp is not None 可以不写
             temp = temp.next
             index -= 1
         return temp.data
 
     def display(self):
-        temp = self.head
+        temp = self.head.next
         while temp is not None:
             print(temp.data)
             temp = temp.next
 
     def index_of(self, x: int) -> int:
-        temp = self.head
+        temp = self.head.next
         ans = 0
         while temp is not None:
             if temp.data == x:
@@ -54,56 +55,51 @@ class LinkList:
             temp = temp.next
         return -1
 
-    def remove_by_value(self, x: int):
+    def remove_by_value(self, x: int) -> int:
         if self.length == 0:
-            return
-        if self.head.data == x:
-            self.head = self.head.next
-            return
+            return 0
         pre = self.head
         cur = self.head.next
         while cur is not None:
             if cur.data == x:
-                pre.next = cur.next
-                break
+                pre.next = cur.next  # 假如cur是最后一个，那它的next已经是head了，不用分情况区分
+                self.length -= 1
+                return 1
             pre = cur
             cur = cur.next
+        return 0
 
     def remove_by_index(self, x: int):
-        if x >= self.length:
+        if x >= self.length or x < 0:
             raise IndexError("Index out of range")
-        if 0 == x:
-            self.head = self.head.next
-            return
+        self.length -= 1
         pre = self.head
         cur = self.head.next
-        index = 1
+        index = 0
         while cur is not None:
             if index == x:
-                pre.next = cur.next
+                pre.next = cur.next  # 假如cur是最后一个，那它的next已经是head了，不用分情况区分，只需要保证满足最后一个的next是head即可
                 break
             pre = cur
             cur = cur.next
             index += 1
 
     def insert_by_index(self, index: int, val: int):
-        if index < 0 or index > self.length + 1:
+        if index < 0 or index > self.length:
             raise IndexError("Index out of range")
         node = ListNode(data=val)
-        if index == 0:
-            node.next = self.head
-            self.head = node
-            return
         temp = self.head
         pos = 0
-        while pos+1 < index:
+        while pos < index:
             temp = temp.next
-        node.next = temp.next
-        temp.next = node
+            pos += 1
+        node.next = temp.next  # 如果是空链表并且插入第一个位置(也是最后一个位置)，temp的next是自身，正好循环了
+        temp.next = node 
+        self.length += 1
 
 
 if __name__ == '__main__':
     link_list = LinkList()
     for i in range(5):
-        link_list.append_first(i)
+        link_list.insert_by_index(i, i)
     link_list.display()

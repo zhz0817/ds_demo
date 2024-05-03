6f73b99d3cb1ca9cc34fc9f1525f4e7dd6e814ce
zhz <zhz15114585761@gmail.com>
2024-05-03 08:46:17 +0800

    双向链表

--- 链表/双向链表/main.py
+++ 链表/双向链表/main.py
@@ -2,50 +2,50 @@ class ListNode:
     def __init__(self, data):
         self.data = data
         self.next = None
+        self.prior = None
 
 
 class LinkList:
     def __init__(self):
-        self.head = None
+        self.head = ListNode(-1)
+        self.tail = self.head
         self.length = 0
 
     def clear(self):
-        self.head = None
+        self.head = ListNode(-1)
+        self.tail = self.head
         self.length = 0
-        # self.__init__() #因为构造函数会返回对象
 
     def is_empty(self):
         return self.length == 0
 
-    def append_first(self, data):
+    def append_last(self, data):
         node = ListNode(data=data)
-        if self.head is None:
-            self.head = node
-        else:
-            node.next = self.head
-            self.head = node
+        self.tail.next = node
+        node.prior = self.tail
+        self.tail = self.tail.next
         self.length += 1
 
-    def get_length(self):
+    def get_length(self) -> int:
         return self.length
 
     def get_value_by_index(self, index) -> int:
-        temp = self.head
         if index < 0 or index >= self.length:
             raise IndexError("Index out of")
+        temp = self.head.next
         while index > 0 and temp is not None:  # temp is not None 可以不写
             temp = temp.next
             index -= 1
         return temp.data
 
     def display(self):
-        temp = self.head
+        temp = self.head.next
         while temp is not None:
             print(temp.data)
             temp = temp.next
 
     def index_of(self, x: int) -> int:
-        temp = self.head
+        temp = self.head.next
         ans = 0
         while temp is not None:
             if temp.data == x:
@@ -54,56 +54,55 @@ class LinkList:
             temp = temp.next
         return -1
 
-    def remove_by_value(self, x: int):
+    def remove_by_value(self, x: int) -> int:
         if self.length == 0:
-            return
-        if self.head.data == x:
-            self.head = self.head.next
-            return
+            return 0
         pre = self.head
         cur = self.head.next
         while cur is not None:
             if cur.data == x:
                 pre.next = cur.next
-                break
+                cur.next.prior = pre
+                self.length -= 1
+                return 1
             pre = cur
             cur = cur.next
+        return 0
 
     def remove_by_index(self, x: int):
-        if x >= self.length:
+        if x >= self.length or x < 0:
             raise IndexError("Index out of range")
-        if 0 == x:
-            self.head = self.head.next
-            return
+        self.length -= 1
         pre = self.head
         cur = self.head.next
-        index = 1
+        index = 0
         while cur is not None:
             if index == x:
                 pre.next = cur.next
+                cur.next.prior = pre
                 break
             pre = cur
             cur = cur.next
             index += 1
 
     def insert_by_index(self, index: int, val: int):
-        if index < 0 or index > self.length + 1:
+        if index < 0 or index > self.length:
             raise IndexError("Index out of range")
         node = ListNode(data=val)
-        if index == 0:
-            node.next = self.head
-            self.head = node
-            return
         temp = self.head
         pos = 0
-        while pos+1 < index:
+        while pos < index:
             temp = temp.next
+            pos += 1
         node.next = temp.next
+        node.prior = temp
         temp.next = node
+        self.length += 1
 
 
 if __name__ == '__main__':
     link_list = LinkList()
     for i in range(5):
-        link_list.append_first(i)
+        link_list.append_last(i)
+    link_list.insert_by_index(5, 5)
     link_list.display()
